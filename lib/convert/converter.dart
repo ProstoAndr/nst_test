de7dc0dda0f6bbfdf3deb4ffe7294339306f8953
prostoAndrey <petuxov26120123@gmail.com>
2023-10-05 17:17:05 +0700

    Изменил конвертер валюты Добавил исключения создал новые тесты

--- lib/convert/converter.dart
+++ lib/convert/converter.dart
@@ -1,5 +1,22 @@
 class Converter {
   double convert(String text, double value) {
-    return double.parse(text) * value;
+    final currencyAmount = double.parse(text);
+
+    final max = BigInt.parse('100000000000000000000000000000');
+    if (currencyAmount < 0) {
+      return valueLessThanZero();
+    } else if (BigInt.parse(text) > max) {
+      return valueIsTooLarge();
+    } else {
+      return currencyAmount * value;
+    }
   }
+}
+
+Never valueLessThanZero() {
+  throw ArgumentError('Value less than zero');
+}
+
+Never valueIsTooLarge() {
+  throw ArgumentError('The value is too large');
 }
--- test/unit_test.dart
+++ test/unit_test.dart
@@ -2,13 +2,34 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:nst_test/convert/converter.dart';
 
 void main() {
+  test('test converter return 188.7312 when adding 62.9104 and 3', () {
+    final converter = Converter();
+
+    final result = converter.convert('3', 62.9104);
+
+    expect(result, 188.7312);
+  });
+
+  test('test converter return 0 when adding 62.9104 and 0', () {
+    final converter = Converter();
+
+    final result = converter.convert('0', 62.9104);
+
+    expect(result, 0);
+  });
+
+  test('test converter return ArgumentError when adding 62.9104 and -10', () {
+    final converter = Converter();
+
+    expect(() => converter.convert('-10.0', 62.9104),
+        throwsA(predicate((e) => e is ArgumentError)));
+  });
+
   test(
-    'test converter return 188.7312 when adding 62.9104 and 3', (){
-      final converter = Converter();
+      'test converter return ArgumentError when adding too large value and 62.9104', () {
+    final converter = Converter();
 
-      final result = converter.convert('62.9104', 3);
-
-      expect(result, 188.7312);
-      }
-  );
+    expect(() => converter.convert('1000000000000000000000000000000', 62.9104),
+        throwsA(predicate((e) => e is ArgumentError)));
+  });
 }
